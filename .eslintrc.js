91cc3ce4fd7cd24f2c243093c322d9937c187041
yuvraj <yuvraj042003@gmail.com>
2023-06-27 08:28:59 +0530

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  'extends': [
+    'plugin:react/recommended'
+],
+  
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'no-unused-vars': 'off',
+    'react-refresh/only-export-components': 'warn',
+  },
+  
+}
